bb18a7235690459069a16a3f120f5fd9020fdc6e
av-dev2 <emgani@aakvatech.com>
2025-11-15 15:03:15 +0300

    feat: add Truck doctype with validation, test cases, and client-side events

---
+++ logis/logis/doctype/truck/test_truck.py
@@ -0,0 +1,45 @@
+# Copyright (c) 2025, Administrator and Contributors
+# See license.txt
+
+import frappe
+from frappe.tests import IntegrationTestCase, UnitTestCase
+
+
+class UnitTestTruck(UnitTestCase):
+	"""
+	Unit tests for Truck.
+	Use this class for testing individual functions and methods.
+	"""
+
+	pass
+
+
+class IntegrationTestTruck(IntegrationTestCase):
+	"""
+	Integration tests for Truck.
+	Use this class for testing interactions with the database.
+	"""
+
+	def setUp(self):
+		"""Set up test data before each test."""
+		pass
+	
+	def tearDown(self):
+		"""Clean up test data after each test."""
+		pass
+	
+	def test_truck_creation(self):
+		"""Test creating a new Truck."""
+		# Create test document
+		doc = frappe.get_doc({
+			"doctype": "Truck",
+			# Add required fields here
+		})
+		doc.insert()
+		
+		# Assertions
+		self.assertEqual(doc.doctype, "Truck")
+		self.assertIsNotNone(doc.name)
+		
+		# Clean up
+		doc.delete()
---
+++ logis/logis/doctype/truck/truck.js
@@ -0,0 +1,24 @@
+// Copyright (c) 2025, Administrator and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Truck", {
+	refresh(frm) {
+		// Called when form is refreshed
+		if (frm.doc.__islocal) {
+			// Document is new
+		} else {
+			// Document exists
+		}
+	},
+	
+	validate(frm) {
+		// Called before document is saved (client-side validation)
+	},
+	
+	onload(frm) {
+		// Called when form is loaded
+	},
+	
+	// Field-specific events
+	// Example: fieldname: function(frm) { }
+});
