846d55615a520233491d99e75d4724ded548cc06
av-dev2 <emgani@aakvatech.com>
2025-11-15 21:21:40 +0300

    feat: implement truck entry creation in Trip Assignment before submission

--- logis/logis/doctype/trip_assignment/trip_assignment.py
+++ logis/logis/doctype/trip_assignment/trip_assignment.py
@@ -1,9 +1,23 @@
 # Copyright (c) 2025, elius mgani and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class TripAssignment(Document):
-	pass
+	def before_submit(self):
+		self.create_truck_entry()
+
+	def create_truck_entry(self):
+		truck_entry = f"{self.truck}/{self.trailer}"
+
+		if not frappe.db.exists("Truck Entry", truck_entry):
+			truck_entry_doc = frappe.new_doc("Truck Entry")
+			truck_entry_doc.truck_name = truck_entry
+			truck_entry_doc.save(ignore_permissions=True)
+
+			self.truck_name = truck_entry_doc.name
+		
+		else:
+			self.truck_name = truck_entry
