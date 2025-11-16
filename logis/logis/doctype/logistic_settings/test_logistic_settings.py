a95dad286bc26c744810d3aaa8d015affdac36f8
av-dev2 <emgani@aakvatech.com>
2025-11-16 23:50:46 +0300

    feat: add Logistic Settings doctype with initial structure and test cases

---
+++ logis/logis/doctype/logistic_settings/test_logistic_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, elius mgani and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLogisticSettings(FrappeTestCase):
+	pass
