18dbbcf8e45997368596ab705b028f7ac0aff900
av-dev2 <emgani@aakvatech.com>
2025-11-16 23:34:54 +0300

    refactor: streamline Logistic Expense form event handlers and set_filters method

--- logis/logis/doctype/logistic_expense/logistic_expense.js
+++ logis/logis/doctype/logistic_expense/logistic_expense.js
@@ -1,8 +1,20 @@
 // Copyright (c) 2025, elius mgani and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Logistic Expense", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Logistic Expense", {
+	refresh: (frm) => {
+        frm.trigger("set_filters");
+	},
+    onload: (frm) => {
+        frm.trigger("set_filters");
+    },
+    set_filters: (frm) => {
+        frm.set_query("expense_account", () => {
+            return {
+                filters: {
+                    root_type: 'Expense',
+                }
+            };
+        })
+    }
+});
