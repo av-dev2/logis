ea85065836bdf1aabac79f8e0a8072eafc42c900
av-dev2 <emgani@aakvatech.com>
2025-12-06 11:48:28 +0300

    feat: streamline Maintenance Request form by removing unused buttons and enhancing spares handling

--- logis/logis/doctype/maintenance_request/maintenance_request.js
+++ logis/logis/doctype/maintenance_request/maintenance_request.js
@@ -2,190 +2,83 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Maintenance Request', {
-	refresh: function(frm) {
-		// Add buttons for actions
-		if (frm.doc.docstatus === 0) {
-			frm.add_custom_button(__('Populate from Template'), function() {
-				if (frm.doc.maintenance_template) {
-					frappe.call({
-						method: 'frappe.client.get',
-						args: {
-							doctype: 'Maintenance Template',
-							name: frm.doc.maintenance_template
-						},
-						callback: function(r) {
-							if (r.message) {
-								const template = r.message;
-								
-								// Update problem category and description
-								frm.set_value('problem_category', template.problem_category);
-								frm.set_value('problem_description', template.description || '');
-								
-								// Clear and populate inspection steps if template has steps
-								if (template.steps && template.steps.length > 0) {
-									frm.set_value('no_of_inspections', template.steps.length);
-									
-									frm.clear_table('inspections');
-									template.steps.forEach(function(step, index) {
-										frm.add_child('inspections', {
-											inspection_no: index + 1,
-											inspection_type: 'Visual Inspection',
-											description: step.description,
-											status: 'Pending'
-										});
-									});
-									frm.refresh_field('inspections');
-									frappe.msgprint(__('Template populated with {0} inspections', [template.steps.length]));
-								} else {
-									frappe.msgprint(__('Template populated successfully'));
-								}
-							}
-						}
-					});
+	setup: (frm) => {
+		frm.trigger('set_query');
+		frm.trigger('validate_add_spare');
+	},
+
+	refresh: (frm) => {
+		frm.trigger('set_query');
+		frm.trigger('validate_add_spare');
+
+		// Show Stock Entry if created
+		if (frm.doc.stock_entry) {
+			frm.add_custom_button(__('View Stock Entry'), function() {
+				frappe.set_route('Form', 'Stock Entry', frm.doc.stock_entry);
+			}, __('View'));
+		}
+	},
+
+	onload: (frm) => {
+		frm.trigger('set_query');
+		frm.trigger('validate_add_spare');
+	},
+	
+	request_spares: (frm) => {
+		if (frm.doc.spares.length === 0) {
+			frappe.msgprint(__('Please add at least one spare part to request.'));
+			return;
+		}
+
+		if (frm.is_dirty()) {
+			frm.save().then(() => {
+				frm.reload_doc();
+			});
+		}
+
+		frappe.call({
+			method: 'create_stock_entry',
+			doc: frm.doc,
+			freeze: true,
+			freeze_message: __('Creating Stock Entry...'),
+			callback: (r) => {
+				if (r.message) {
+					frm.reload_doc();
+					frappe.msgprint(__('Stock Entry {0} created successfully.', [r.message]));
 				} else {
-					frappe.msgprint(__('Please select a Maintenance Template first'));
+					frappe.msgprint(__('No spare parts were requested.'));
 				}
-			}, __('Maintenance'));
-		}
-
-		// Show Material Request if created
-		if (frm.doc.material_request) {
-			frm.add_custom_button(__('View Material Request'), function() {
-				frappe.set_route('Form', 'Material Request', frm.doc.material_request);
-			}, __('Material Request'));
-		}
-
-		// Auto-fill vehicle details when vehicle is selected
-		if (frm.doc.vehicle) {
-			frappe.call({
-				method: 'frappe.client.get',
-				args: {
-					doctype: frm.doc.vehicle_type,
-					name: frm.doc.vehicle
-				},
-				callback: function(r) {
-					if (r.message) {
-						frm.set_value('license_plate', r.message.license_plate);
-						frm.set_value('make', r.message.make);
-						frm.set_value('model', r.message.model);
-					}
-				}
-			});
-		}
-	},
-
-	vehicle_type: function(frm) {
-		// Update vehicle field filter based on vehicle type
-		if (frm.doc.vehicle_type === 'Truck') {
-			frm.fields_dict.vehicle.get_query = function() {
-				return {
-					filters: {
-						docstatus: 0
-					}
-				};
-			};
-		} else if (frm.doc.vehicle_type === 'Trailer') {
-			frm.fields_dict.vehicle.get_query = function() {
-				return {
-					filters: {
-						doctype: 'Trailer',
-						docstatus: 0
-					}
-				};
-			};
-		}
-		// Clear vehicle field when type changes
-		frm.set_value('vehicle', null);
-		frm.set_value('license_plate', '');
-		frm.set_value('make', '');
-		frm.set_value('model', '');
-	},
-
-	vehicle: function(frm) {
-		// Auto-fill vehicle details
-		if (frm.doc.vehicle) {
-			frappe.call({
-				method: 'frappe.client.get',
-				args: {
-					doctype: frm.doc.vehicle_type,
-					name: frm.doc.vehicle
-				},
-				callback: function(r) {
-					if (r.message) {
-						frm.set_value('license_plate', r.message.license_plate);
-						frm.set_value('make', r.message.make);
-						frm.set_value('model', r.message.model);
-					}
-				}
-			});
-		}
-	},
-
-	maintenance_template: function(frm) {
-		// Show option to populate from template
-		if (frm.doc.maintenance_template) {
-			frappe.msgprint(__('Click "Populate from Template" button to auto-fill problem details'));
-		}
-	},
-
-	no_of_inspections: function(frm) {
-		// Auto-create inspection rows
-		const current_count = frm.doc.inspections.length;
-		const required_count = frm.doc.no_of_inspections;
-
-		if (required_count > current_count) {
-			for (let i = current_count; i < required_count; i++) {
-				frm.add_child('inspections', {
-					inspection_no: i + 1,
-					status: 'Pending'
-				});
 			}
-		} else if (required_count < current_count) {
-			// Remove extra rows
-			for (let i = frm.doc.inspections.length - 1; i >= required_count; i--) {
-				frm.get_field('inspections').grid.df.data.splice(i, 1);
-			}
-		}
+		});
+	},
+	set_query: (frm) => {
+		frm.set_query('spare', 'spares', () => {
+			return {
+				filters: {
+					is_stock_item: 1
+				}
+			};
+		});
+	},
+	validate_add_spare: (frm) => {
+		if (frm.doc.stock_entry) {
+			frm.get_field("spares").grid.cannot_add_rows = true;
 
-		frm.refresh_field('inspections');
-	}
+			$("*[data-fieldname='spares']").find(".grid-remove-rows").hide();
+			$("*[data-fieldname='spares']").find(".grid-remove-all-rows").hide();
+		}
+	},
 });
 
 // Add auto-fetch for item details in spares table
-frappe.ui.form.on('Maintenance Spare', {
-	item_code: function(frm, cdt, cdn) {
-		let row = locals[cdt][cdn];
-		if (row.item_code) {
-			frappe.call({
-				method: 'frappe.client.get',
-				args: {
-					doctype: 'Item',
-					name: row.item_code
-				},
-				callback: function(r) {
-					if (r.message) {
-						frappe.model.set_value(cdt, cdn, 'item_name', r.message.item_name);
-						frappe.model.set_value(cdt, cdn, 'unit_cost', r.message.standard_rate || 0);
-						cur_frm.refresh_field('spares');
-					}
-				}
-			});
+frappe.ui.form.on('Maintenance Spare Detail', {
+	form_render: (frm, cdt, cdn) => {
+		if (frm.doc.stock_entry) {
+			frm.fields_dict.spares.grid.wrapper.find(".grid-delete-row").hide();
+			frm.fields_dict.spares.grid.wrapper.find(".grid-insert-row-below").hide();
+			frm.fields_dict.spares.grid.wrapper.find(".grid-insert-row").hide();
+			frm.fields_dict.spares.grid.wrapper.find(".grid-duplicate-row").hide();
+			frm.fields_dict.spares.grid.wrapper.find(".grid-move-row").hide();
 		}
 	},
-
-	quantity_required: function(frm, cdt, cdn) {
-		let row = locals[cdt][cdn];
-		if (row.quantity_required && row.unit_cost) {
-			frappe.model.set_value(cdt, cdn, 'total_cost', row.quantity_required * row.unit_cost);
-			cur_frm.refresh_field('spares');
-		}
-	},
-
-	unit_cost: function(frm, cdt, cdn) {
-		let row = locals[cdt][cdn];
-		if (row.quantity_required && row.unit_cost) {
-			frappe.model.set_value(cdt, cdn, 'total_cost', row.quantity_required * row.unit_cost);
-			cur_frm.refresh_field('spares');
-		}
-	}
 });
